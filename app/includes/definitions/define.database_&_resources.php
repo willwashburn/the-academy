a54592bccb6ca66c9873e4f16528d21cb6b24c3b
Will Washburn <will@winston>
2012-07-03 12:48:50 -0400

    removed resources from definitions

--- app/includes/definitions/define.database_&_resources.php
+++ app/includes/definitions/define.database_&_resources.php
@@ -9,17 +9,13 @@
 	switch (__ENVIRONMENT) {
 		case 'production':
 			$database = $databases['production'];
-			$resources_path = $resources_paths['production'];
 			break;
 		case 'dev':
 			$database = $databases['dev'];
-			$resources_path = $resources_paths[__DOMAIN_PREFIX];
 			break;
 	}
 
 	//WBN add a sanity isset check
-
-	define('__RESOURCES_PATH', $resources_path);
 
 	define('__DB_USER', $db_username);
 	define('__DB_PW', $db_password);
