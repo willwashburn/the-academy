31aaff5deb1d1999256efd65097afa8ebdb8921b
Will Washburn <will@winston>
2012-07-13 12:39:01 -0400

    added host name to definitions

--- app/includes/definitions/define.environment.php
+++ app/includes/definitions/define.environment.php
@@ -1,30 +1,34 @@
 <?php
-	/*
-	 * What environment are we in?
-	 * @use are we in dev mode or staging or production
-	 * @oa    Will
-	 *
-	 * basically, is one of the dev prefixes prepended to the base domain?
-	 *
-	 */
+    /*
+      * What environment are we in?
+      * @use are we in dev mode or staging or production
+      * @oa    Will
+      *
+      * basically, is one of the dev prefixes prepended to the base domain?
+      *
+      */
 
-	$environment   = 'production';
-	$domain_prefix = '';
+    $environment   = 'production';
+    $domain_prefix = '';
 
-	foreach ($dev_prefixes as $prefix) {
+    foreach ($dev_prefixes as $prefix) {
 
-		$dev_domain = $prefix . '.' . $base_domain;
+        $dev_domain = $prefix . '.' . $base_domain;
 
-		$dev_domain_position = strpos($_SERVER['HTTP_HOST'], $dev_domain);
+        $dev_domain_position = strpos($_SERVER['HTTP_HOST'], $dev_domain);
 
-		if ($dev_domain_position != 0 || $dev_domain === $_SERVER['HTTP_HOST']) {
-			$base_domain   = $dev_domain;
-			$domain_prefix = $prefix;
-			$environment   = 'dev';
+        if ($dev_domain_position != 0 || $dev_domain === $_SERVER['HTTP_HOST']) {
+            $base_domain   = $dev_domain;
+            $domain_prefix = $prefix;
+            $environment   = 'dev';
 
-			$dev_domain_position--;
-		}
-	}
+            $dev_domain_position--;
+        }
+    }
 
-	define('__DOMAIN_PREFIX', $domain_prefix);
-	define('__ENVIRONMENT', $environment);
+    $host_name = getHostByName(getHostName()); //returns host ip ie 69.164.222.13
+
+
+    define('__DOMAIN_PREFIX', $domain_prefix);
+    define('__ENVIRONMENT', $environment);
+    define('__HOST_NAME', $host_name);
