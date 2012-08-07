81ce8a0e431bb68da0bfe20a76e1ff747acb6a8c
Will Washburn <will@winston>
2012-08-07 16:00:51 -0400

    added an academy AR extension so we can make more granular changes specific to projects

---
+++ app/helpers/model.helper.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * ActiveRecord Model extension for PHP
+ * @author      Will
+ * @description Extend AR instead of making changes to the main AR class that we only want for this project
+ */
+
+    namespace academy;
+
+class model extends \ActiveRecord\model {
+
+}
