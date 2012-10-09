4fec4d77d772a96f57723a9be423dc91d2480f92
Will Washburn <will@alfred>
2012-10-09 19:53:53 -0400

    add multimodel to basic set

--- app/helpers/model.helper.php
+++ app/helpers/model.helper.php
@@ -1,13 +1,8 @@
 <?php
 
-/**
- * ActiveRecord Model extension for PHP
- * @author      Will
- * @description Extend AR instead of making changes to the main AR class that we only want for this project
- */
-
     namespace academy;
 
-class model extends \ActiveRecord\model {
+    class model extends \ActiveRecord\model
+    {
 
-}
+    }
---
+++ app/helpers/multimodel.helper.php
@@ -0,0 +1,8 @@
+<?php
+
+    namespace academy;
+
+    class multiModel extends \ActiveRecord\MultiModel
+    {
+
+    }
